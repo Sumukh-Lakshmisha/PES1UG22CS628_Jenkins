9d4d9852f7c7d92ab933ed271e8ae94722a22d75
Sumukh-Lakshmisha <Sumukh-Lakshmisha>
2025-03-16 11:43:16 +0530

    cs628

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,8 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I am PES1UG22CS628\n";
-  cout << " I like it!\n";
-  cout << " woww!\n";
+  cout << " wowww!\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
