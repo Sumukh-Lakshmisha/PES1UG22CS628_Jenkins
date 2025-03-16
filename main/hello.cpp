ae4d8fe7231225617dfb57409b21d457344c2b72
Sumukh-Lakshmisha <Sumukh-Lakshmisha>
2025-03-16 11:45:58 +0530

    india

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I am PES1UG22CS628\n";
-  cout << " wowww!\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
